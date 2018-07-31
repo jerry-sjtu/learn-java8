94f902e5c44ad237af8063b3dd1bb22245cd3192
qiang.wang <qiang.wang@meituan.com>
2018-07-31 11:44:21 +0800

    primitive feature is added.

--- src/main/java/org/dcharm/java/feature/FeatureWithMapDB.java
+++ src/main/java/org/dcharm/java/feature/FeatureWithMapDB.java
@@ -1,5 +1,8 @@
 package org.dcharm.java.feature;
 
+import com.carrotsearch.hppc.IntFloatHashMap;
+import com.carrotsearch.hppc.IntLongHashMap;
+import com.sun.xml.internal.fastinfoset.util.StringIntMap;
 import org.mapdb.*;
 
 import java.util.HashMap;
@@ -14,11 +17,13 @@ public class FeatureWithMapDB {
     public static void main(String[] args) {
         long t1 = System.currentTimeMillis();
         Map map = mapWrite1();
+//        IntFloatHashMap map = mapWrite2();
         System.out.println(System.currentTimeMillis() - t1);
         System.out.println("----------");
 
         t1 = System.currentTimeMillis();
         mapRead(map);
+//        mapRead2(map);
         System.out.println(System.currentTimeMillis() - t1);
         System.out.println("----------");
     }
@@ -37,16 +42,26 @@ public class FeatureWithMapDB {
         return map;
     }
 
-    private static Map<String, Long> mapWrite1() {
+    private static Map<Integer, Float> mapWrite1() {
         //in memory hash map
 //        DB db = DBMaker.memoryDB().make();
 //        DB db = DBMaker.heapDB().make();
         //off-heap store
 //        ConcurrentMap<String, Long> map = db.hashMap("map", Serializer.STRING, Serializer.LONG).createOrOpen();
-        Map<String, Long> map = new HashMap<>(1000000);
+        Map<Integer, Float> map = new HashMap<>(1000000);
 //        Map<String, Long> map = new TreeMap<>();
-        for(long i = 0; i < 1000000L; i++) {
-            map.put(String.valueOf(i), i);
+        for(int i = 0; i < 1000000; i++) {
+            map.put(i, (float)i);
+        }
+
+//        db.close();
+        return map;
+    }
+
+    private static IntFloatHashMap mapWrite2() {
+        IntFloatHashMap map = new IntFloatHashMap(1000000);
+        for(int i = 0; i < 1000000; i++) {
+            map.put(i, i);
         }
 
 //        db.close();
@@ -54,16 +69,29 @@ public class FeatureWithMapDB {
     }
 
 
-
-    private static void mapRead(Map<String, Long> map) {
+    private static void mapRead(Map<Integer, Float> map) {
 //        DB db = DBMaker.memoryDB().make();
 //        //off-heap store
 //        ConcurrentMap map = db.hashMap("map", Serializer.STRING, Serializer.LONG).open();
-        for(long i = 0; i < 1000000L; i++) {
-            map.get(String.valueOf(i));
+        for(int i = 0; i < 1000000L; i++) {
+            map.get(i);
         }
-        System.out.println(map.get("100"));
+        System.out.println(100);
 //        db.close();
     }
+
+
+    private static void mapRead2(IntFloatHashMap map) {
+//        DB db = DBMaker.memoryDB().make();
+//        //off-heap store
+//        ConcurrentMap map = db.hashMap("map", Serializer.STRING, Serializer.LONG).open();
+        for(int i = 0; i < 1000000; i++) {
+            map.get(i);
+        }
+        System.out.println(100);
+//        db.close();
+    }
+
+
 
 }
